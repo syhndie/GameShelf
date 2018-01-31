16272e3f29388875da6c03ef300210ff29b06ffa
Cindy <syhndie2@gmail.com>
2018-01-31 16:44:28 -0600

    Added title filter and owner filter to games index page

--- GameShelf/Controllers/GamesController.cs
+++ GameShelf/Controllers/GamesController.cs
@@ -20,9 +20,9 @@ namespace GameShelf.Controllers
         }
 
         // GET: Games
-        public IActionResult Index(string titleFilter)
+        public IActionResult Index(string titleFilter, string ownerFilter)
         {
-            var indexVM = new GameIndexViewModel(_context);
+            var indexVM = new GameIndexViewModel(_context, titleFilter, ownerFilter);
 
             return View(indexVM);
         }
--- GameShelf/Models/GameIndexViewModel.cs
+++ GameShelf/Models/GameIndexViewModel.cs
@@ -10,9 +10,13 @@ namespace GameShelf.Models
     {
         public List<GameWithPersonInfo> GameList { get; set; }
         public string TitleFilter { get; set; }
+        public string OwnerFilter { get; set; }
 
-        public GameIndexViewModel (GameShelfContext db)
+        public GameIndexViewModel (GameShelfContext db, string titleFilter, string ownerFilter)
         {
+            TitleFilter = titleFilter;
+            OwnerFilter = ownerFilter;
+
             var gamesIEnum = db.Games
                 .Include(g => g.GamePersonRelationships)
                 .ThenInclude(gpr => gpr.Person)
@@ -24,11 +28,18 @@ namespace GameShelf.Models
                     Owners = g.GamePersonRelationships.Where(gpr => gpr.Role == "Owner").Select(gpr => gpr.Person).ToList(),
                     Designers = g.GamePersonRelationships.Where(gpr => gpr.Role == "Designer").Select(gpr => gpr.Person).ToList()
                 });
+
+            if (!String.IsNullOrEmpty(titleFilter))
+            {
+                gamesIEnum = gamesIEnum.Where(gpi => gpi.Title.ToLower().Contains(titleFilter.ToLower()));
+            } 
+
+            if (!String.IsNullOrEmpty(ownerFilter))
+            {
+                gamesIEnum = gamesIEnum.Where(gpi => gpi.Owners.Any(o => o.FullName.ToLower().Contains(ownerFilter.ToLower())));
+            }
+
             GameList = gamesIEnum.ToList();
         }
-        //    if (!String.IsNullOrEmpty(titleFiler))
-        //    {
-        //        gamesIEnum = gamesIEnum.Where(g => g.Title.Contains(titleFiler));
-        //    }
     }
 }
