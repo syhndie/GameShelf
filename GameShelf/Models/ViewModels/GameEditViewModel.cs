1db98c231eeb117f8f7a9d3ad86eec50adac61a7
Cindy <syhndie2@gmail.com>
2018-02-17 15:53:55 -0600

    started work on new game edit page

---
+++ GameShelf/Models/ViewModels/GameEditViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameShelf.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GameShelf.Models.ViewModels
+{
+    public class GameEditViewModel
+    {
+        public GameWithPersonInfo GameWithPersonInfo { get; set; }
+        public SelectList PlayTimeSelect { get; set; }
+
+        public GameEditViewModel(GameShelfContext db)
+        {
+            var playTimeQuery = from pt in db.Playtimes
+                                orderby pt.ID
+                                select pt;
+            PlayTimeSelect = new SelectList(playTimeQuery, "PlayTimeCategory", "PlayTimeCategory");
+        }
+    }
+}
